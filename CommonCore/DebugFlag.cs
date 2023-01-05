5657f63c6284505b8550304b9d6e7f1e74e440a0
Tony Bierman <tonybierman@gmail.com>
2023-01-05 13:30:19 -0600

    DebugFlag

---
+++ CommonCore/DebugFlag.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CommonCore
+{
+    public sealed class DebugFlag
+    {
+
+        public bool IsDebug { get; set; }
+
+        private DebugFlag()
+        {
+        }
+
+        private static readonly Lazy<DebugFlag> lazy = new Lazy<DebugFlag>(() => new DebugFlag());
+        
+        public static DebugFlag Instance
+        {
+            get
+            {
+                return lazy.Value;
+            }
+        }
+    }
+}
